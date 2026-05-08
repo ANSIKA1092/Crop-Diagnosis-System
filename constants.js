7d02fc6ac374905cfedea53e464f991d41e299e7
ANSIKA1092 <2403A51092@sru.edu.in>
2026-05-08 22:05:50 +0530

    Add files via upload

--- constants.js
+++ constants.js
@@ -1,19 +1,73 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OP_MSG = exports.OP_COMPRESSED = exports.OP_DELETE = exports.OP_QUERY = exports.OP_INSERT = exports.OP_UPDATE = exports.OP_REPLY = exports.MIN_SUPPORTED_RAW_DATA_SERVER_VERSION = exports.MIN_SUPPORTED_RAW_DATA_WIRE_VERSION = exports.MIN_SUPPORTED_QE_SERVER_VERSION = exports.MIN_SUPPORTED_QE_WIRE_VERSION = exports.MAX_SUPPORTED_WIRE_VERSION = exports.MIN_SUPPORTED_WIRE_VERSION = exports.MAX_SUPPORTED_SERVER_VERSION = exports.MIN_SUPPORTED_SERVER_VERSION = void 0;
-exports.MIN_SUPPORTED_SERVER_VERSION = '4.2';
-exports.MAX_SUPPORTED_SERVER_VERSION = '8.2';
-exports.MIN_SUPPORTED_WIRE_VERSION = 8;
-exports.MAX_SUPPORTED_WIRE_VERSION = 27;
-exports.MIN_SUPPORTED_QE_WIRE_VERSION = 21;
-exports.MIN_SUPPORTED_QE_SERVER_VERSION = '7.0';
-exports.MIN_SUPPORTED_RAW_DATA_WIRE_VERSION = 27;
-exports.MIN_SUPPORTED_RAW_DATA_SERVER_VERSION = '8.2';
-exports.OP_REPLY = 1;
-exports.OP_UPDATE = 2001;
-exports.OP_INSERT = 2002;
-exports.OP_QUERY = 2004;
-exports.OP_DELETE = 2006;
-exports.OP_COMPRESSED = 2012;
-exports.OP_MSG = 2013;
-//# sourceMappingURL=constants.js.map
+'use strict';
+
+/*!
+ * ignore
+ */
+
+const queryOperations = Object.freeze([
+  // Read
+  'countDocuments',
+  'distinct',
+  'estimatedDocumentCount',
+  'find',
+  'findOne',
+  // Update
+  'findOneAndReplace',
+  'findOneAndUpdate',
+  'replaceOne',
+  'updateMany',
+  'updateOne',
+  // Delete
+  'deleteMany',
+  'deleteOne',
+  'findOneAndDelete'
+]);
+
+exports.queryOperations = queryOperations;
+
+/*!
+ * ignore
+ */
+
+const queryMiddlewareFunctions = queryOperations.concat([
+  'validate'
+]);
+
+exports.queryMiddlewareFunctions = queryMiddlewareFunctions;
+
+/*!
+ * ignore
+ */
+
+const aggregateMiddlewareFunctions = [
+  'aggregate'
+];
+
+exports.aggregateMiddlewareFunctions = aggregateMiddlewareFunctions;
+
+/*!
+ * ignore
+ */
+
+const modelMiddlewareFunctions = [
+  'bulkWrite',
+  'createCollection',
+  'insertMany'
+];
+
+exports.modelMiddlewareFunctions = modelMiddlewareFunctions;
+
+/*!
+ * ignore
+ */
+
+const documentMiddlewareFunctions = [
+  'validate',
+  'save',
+  'remove',
+  'updateOne',
+  'deleteOne',
+  'init'
+];
+
+exports.documentMiddlewareFunctions = documentMiddlewareFunctions;
